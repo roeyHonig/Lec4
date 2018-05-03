3b3c3ec254382a8c2772b8eed82290b4c7cbae2e
roeyHonig <roeyhonig94@gmail.com>
2018-05-03 11:49:32 +0300

    Structers in Swift

--- Lec4/ViewController.swift
+++ Lec4/ViewController.swift
@@ -16,12 +16,47 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Reference Types
+        var p1 = CompassPoint(x: 10, y: 20)
+        var p2 = CompassPoint(x: 0, y: 0)
+        
+        print("p1: \(p1.description)")
+        print("p2: \(p2.description)")
+        print("more simple code")
+        print("p1: \(p1)")
+        print("p2: \(p2)")
+        
+        p2 = p1 // now p2 points to whatever value p1 points to
+        p2.x = 1000 // now, both p1.x and p2.x have the value 1000, because they point at the same thing
+        
+        print("p1: \(p1)")
+        print("p2: \(p2)")
+        
+        
+        
+        let person = Person(peoplelFood: "orange", firstName: "roey")
+        print(person)
+        sayGreetTo(p: person) // !!!! mutating function
+        print(person)
+        
+        
+        
+        //End of viewDidLoad func
     }
 
+    func sayGreetTo(p: Person){
+        // Hello Mr.
+        p.firstName = "Mr. " + p.firstName
+        print("Hello, \(p.firstName)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
@@ -30,6 +65,9 @@ class ViewController: UIViewController {
 // because of inheritant. in swift there is no top-level father classs
 // so if we want to provide some written description of our class instatnce we should implement the protocal (Interface)
 // that carries that method
+// it's a bit tricky, sometimes to know for a sure, if the 1st name after the ":" is a super class
+// or an protocall
+// the others will be protoccols
 class Person: Mamal, CustomStringConvertible, Cubaser{
     func pitchUp() {
         
@@ -39,7 +77,7 @@ class Person: Mamal, CustomStringConvertible, Cubaser{
     }
     
     var description: String{
-        return firstName
+        return firstName + "Eats: " + self.food
         
     }
     
@@ -76,7 +114,7 @@ protocol Cubaser {
 
 //Types in Swift: ?
 //enum, protocol, class,?
-
+// class are Reference Type  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 class CompassPoint: CustomStringConvertible{
     var x: Double
     var y: Double
@@ -90,7 +128,32 @@ class CompassPoint: CustomStringConvertible{
         self.y = y
     }
     
+    //Actions
     
 }
+
+
+//Structures are value Types  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+struct Spoint {
+    var x: Double
+    var y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    var description: String{
+        return "x: \(x) , y: \(y)"
+    }
+}
+
+
+
+
+
+
+
+
 
 
